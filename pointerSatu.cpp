b50800724edbcf9e115d4dac04ffba1b34f0e20e
Indra2512 <mononeon1225@gmail.com>
2025-05-05 15:27:40 +0700

    menambahkan pointer reference

---
+++ pointerSatu.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa{
+    public:
+    int nim;
+    void showNim(){
+        cout << "No Induk = " << nim << endl;
+    }
+};
+
+int main (){
+
+    mahasiswa mhs; //object mhs
+    mhs.nim = 5;
+    mhs.showNim(); //member access operator
+
+    mahasiswa &refMhs = mhs; //pointer reference refMhs
+    
+}
