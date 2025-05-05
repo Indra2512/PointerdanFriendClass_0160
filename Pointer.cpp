6a0f54d8016f8210cd6c679f83375b3fa8c6ff46
Indra2512 <mononeon1225@gmail.com>
2025-05-05 15:22:26 +0700

    menambahkan alamat memori

--- Pointer.cpp
+++ Pointer.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
 using namespace std;
 
-class Dosen{
-    public:
+class Dosen
+{
+public:
     string nama;
-    void tampilNama(){
+    void tampilNama()
+    {
         cout << "Namanya adalah " << nama << endl;
     }
 };
-class Staf{
-    public:
+class Staf
+{
+public:
     int nidn;
 };
-int main (){
+int main()
+{
     Dosen ds;
     ds.nama = "giga";
     ds.tampilNama();
+    Dosen &dsref = ds;
+    dsref.nama = "joko";
+    cout << "Alamat memori = " << &dsref << endl;
+    dsref.tampilNama();
+
+    Dosen *pds = &ds;
+    pds->nama = "reza";
+    cout << "Alamat memori = " << pds << endl;
+    pds->tampilNama();
+
+    int a = 5;
+    int b = 3;
+    int *c = &a;
+    *c = 9;
+    cout << endl;
+    cout << a << endl;
+
+    cout << "alamat memori a = " << &a << endl;
+    cout << "alamat memori c = " << c << endl;
+    cout << "cetak c = " << *c << endl;
+    c = &b;
+    cout << "alamat memori c = " << c << endl;
+    cout << "cetak c = " << *c << endl;
 }
